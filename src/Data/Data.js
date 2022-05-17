729419910fdce1013e6a96ebfa3bc4d843e7c6cd
Hamza-md <hz.zahoor@gmail.com>
2022-05-17 23:17:27 +0500

    first Commit

---
+++ src/Data/Data.js
@@ -0,0 +1,38 @@
+import {
+    UilEstate,
+    UilClipboardAlt,
+    UilUsersAlt,
+    UilPackage,
+    UilChart,
+    // UilSignoutAlt,
+   
+}  from "@iconscout/react-unicons";
+
+
+// Sidebar Data
+export const SidebarData = [
+    {
+        icon: UilEstate,
+        heading: "Dashboard",
+    },
+    {
+        icon: UilClipboardAlt,
+        heading: "Orders",
+    },
+    {
+        icon: UilUsersAlt,
+        heading: "Customers",
+    },
+    {
+        icon: UilPackage,
+        heading: "Products",
+    },
+    {
+        icon: UilChart,
+        heading: "Analytics",
+    },
+
+  
+
+    
+];
